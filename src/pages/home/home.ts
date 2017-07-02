2a2236d6972c2b61dd175d82a4effcac33e3f173
root <root@localhost.localdomain>
2017-07-02 10:14:27 +0300

    Welcome to ionic

---
+++ src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+	public name;
+
+  constructor(public navCtrl: NavController) {
+  	this.name = "Moses Mwongela";
+  }
+
+}
